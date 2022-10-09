226f0884abc409507c1ecf12a85f4617cc5860e9
dismke <info@zoobli.io>
2022-10-09 17:28:24 +0200

    Update check-for-deprecation.sh

--- check-for-deprecation.sh
+++ check-for-deprecation.sh
@@ -6,7 +6,7 @@ OUT_FILE=deprecated.txt
 if [ "$1" = "--help" ] || [ "$1" = "-h" ] || [ "$1" = "help" ]; then
   cat <<EOF
 $ check-for-deprecation.sh
-Check all github apps in the readme for deprecation.
+Check all CLI apps in the readme for deprecation.
 Running $J processes in parallel.
 Found deprecated repos are written to: $OUT_FILE
 EOF
@@ -17,7 +17,7 @@ APPS=$(cat readme.md | grep -e "- \[.\+\]\(.\+\)\s" | grep 'github.com' | awk -F
 
 check_deprecation() {
   REPO="$1"
-  if nice curl -SsL "$REPO" | grep "This repository has been archived by the owner. It is now read-only." >/dev/null; then
+  if nice curl -SsL "$REPO" | grep "This repository has been archived by the owner." >/dev/null; then
     echo "DEPRECATED $REPO" | tee -a $OUT_FILE
   else
     echo "CHECKED $REPO"
